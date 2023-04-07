b8b2f58920f85de4aca6955145f6cfd8d4b8e5ef
Raphael Lehmann <raphael@rleh.de>
2023-04-07 14:54:47 +0200

    fixup: EXAMPLE samv71_xplained_ultra can_dual

--- examples/samv71_xplained_ultra/can_dual/main.cpp
+++ examples/samv71_xplained_ultra/can_dual/main.cpp
@@ -32,37 +32,41 @@ public:
 		PT_BEGIN();
 		for (i = 0; i < testMessages.size(); i++)
 		{
+			MODM_LOG_INFO << "Mcan1: Trying to send message (" << testMessages[i] << ") ...\n";
 			while (not Mcan1::sendMessage(testMessages[i])) {
-				MODM_LOG_INFO << "Unable to send message " << testMessages[i] << "on Mcan1\n";
+				MODM_LOG_INFO << "Mcan1: Unable to send message on Mcan1\n";
 				PT_YIELD();
 			}
-			modm::delay(delay);
 			PT_YIELD();
+			MODM_LOG_INFO << "Mcan1: ...success!\n";
+			//modm::delay(delay);
+			//PT_YIELD();
 		}
 		PT_END();
 	}
 
 private:
-	uint8_t messageLength = 64;
+	static constexpr uint8_t messageLength = 64;
+	const std::array<uint8_t, messageLength> messageData{0xaa};
 	std::array<Message, 13> testMessages = {
-		Message{0x10, messageLength, 0x0000000000000010, false},
-		Message{0x11, messageLength, 0x1111111111110011, false},
-		Message{0x12, messageLength, 0x2222222222220012, false},
-		Message{0x13, messageLength, 0x3333333333330013, false},
-		Message{0x14, messageLength, 0x4444444444440014, false},
-		Message{0x15, messageLength, 0x5555555555550015, false},
-		Message{0x16, messageLength, 0x6666666666660016, false},
-		Message{0x15, messageLength, 0x7777777777770015, false},
-		Message{0x14, messageLength, 0x8888888888880014, false},
-		Message{0x13, messageLength, 0x9999999999990013, false},
-		Message{0x12, messageLength, 0xaaaaaaaaaaaa0012, false},
-		Message{0x11, messageLength, 0xbbbbbbbbbbbb0011, false},
-		Message{0x10, messageLength, 0xcccccccccccc0010, false},
+		Message{0x10, messageLength, messageData.data(), false},
+		Message{0x11, messageLength, messageData.data(), false},
+		Message{0x12, messageLength, messageData.data(), false},
+		Message{0x13, messageLength, messageData.data(), false},
+		Message{0x14, messageLength, messageData.data(), false},
+		Message{0x15, messageLength, messageData.data(), false},
+		Message{0x16, messageLength, messageData.data(), false},
+		Message{0x15, messageLength, messageData.data(), false},
+		Message{0x14, messageLength, messageData.data(), false},
+		Message{0x13, messageLength, messageData.data(), false},
+		Message{0x12, messageLength, messageData.data(), false},
+		Message{0x11, messageLength, messageData.data(), false},
+		Message{0x10, messageLength, messageData.data(), false},
 	};
 
 	size_t i;
 
-	std::chrono::microseconds delay = std::chrono::microseconds{1};
+	std::chrono::nanoseconds delay = std::chrono::nanoseconds{1};
 };
 
 template<class Can, uint8_t id>
@@ -77,6 +81,7 @@ public:
 		{
 			PT_WAIT_UNTIL(Can::isMessageAvailable());
 			MODM_LOG_INFO << "Mcan" << id << ": Message is available... ";
+			__DSB();
 			if (Can::getMessage(rxMsg))
 				MODM_LOG_INFO << rxMsg << modm::endl;
 			else
@@ -98,9 +103,13 @@ main()
 
 	MODM_LOG_INFO << "CAN Dual Test Program" << modm::endl;
 
-	MODM_LOG_INFO << "Mcan1: Initializing with 125kbps for boards CAN transceiver (PC12/PC14)." << modm::endl;
+	MODM_LOG_INFO << "Mcan1: Initializing with 125kbps / 1Mbps for boards CAN transceiver (PC12/PC14)." << modm::endl;
 	// Mcan1 is connted in Board::initialize(); CAN transceiver on the dev board
-	Mcan1::initialize<Board::SystemClock, 125_kbps, 1_pct>(12);
+	Mcan1::initialize<Board::SystemClock, 125_kbps, 1_pct, 1_Mbps>(12);
+
+	Mcan1::setErrorInterruptCallback([](){
+		Board::Led1::set();
+	});
 
 	MODM_LOG_INFO << "Mcan1: Setting up Filter to receive every message." << modm::endl;
 	Mcan1::setExtendedFilter(0, Mcan1::FilterConfig::Fifo0,
@@ -110,9 +119,13 @@ main()
 			modm::can::StandardIdentifier(0),
 			modm::can::StandardMask(0));
 
-	MODM_LOG_INFO << "Mcan0: Initializing with 125kbps for PB2/PB3." << modm::endl;
+	MODM_LOG_INFO << "Mcan0: Initializing with 125kbps / 1Mbps for PB2/PB3." << modm::endl;
 	Mcan0::connect<GpioB2::Tx, GpioB3::Rx>();
-	Mcan0::initialize<Board::SystemClock, 125_kbps, 1_pct>(12);
+	Mcan0::initialize<Board::SystemClock, 125_kbps, 1_pct, 1_Mbps>(12);
+
+	Mcan0::setErrorInterruptCallback([](){
+		Board::Led0::set();
+	});
 
 	MODM_LOG_INFO << "Mcan0: Setting up Filter to receive every message." << modm::endl;
 	Mcan0::setExtendedFilter(0, Mcan0::FilterConfig::Fifo0,
